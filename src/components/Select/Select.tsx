a969693d350968ea22f33e0b4af5959dffcb3b47
blackparadise0407 <blackparadise0407@gmail.com>
2022-09-11 23:35:14 +0700

    Implement select input search function

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,13 @@
 import clsx from 'clsx'
-import { forwardRef, useEffect, useRef, useState } from 'react'
+import {
+  ChangeEventHandler,
+  forwardRef,
+  ReactNode,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 
 import { useOnClickOutside } from '@/hooks/useOnClickOutside'
@@ -13,7 +21,9 @@ interface SelectProps {
   options?: Array<Option>
   defaultOpen?: boolean
   placeholder?: string
+  enableSearch?: boolean
   value?: string | number | readonly string[]
+  emptyContent?: ReactNode
   onChange?: (value?: string | number | readonly string[]) => void
 }
 
@@ -23,6 +33,8 @@ export default forwardRef<HTMLDivElement, SelectProps>(function Select(
     defaultOpen = false,
     value,
     placeholder,
+    enableSearch = true,
+    emptyContent,
     onChange = () => {},
   }: SelectProps,
   ref
@@ -31,16 +43,36 @@ export default forwardRef<HTMLDivElement, SelectProps>(function Select(
   const [open, setOpen] = useState(defaultOpen)
   const divRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
+  const selectDivRef = useRef<HTMLInputElement>(null)
+  const [searchTerm, setSearchTerm] = useState('')
 
   useOnClickOutside(divRef, () => {
     setOpen(false)
   })
 
-  const selectedOptionsIdx = options.findIndex((it) => it.value === innerVal)
+  const handleSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const derivedOptions = useMemo(() => {
+    if (!searchTerm) return options
+    return options.filter((it) => new RegExp(searchTerm).test(it.label))
+  }, [searchTerm, options])
+
+  const selectedOptionsIdx = derivedOptions.findIndex(
+    (it) => it.value === innerVal
+  )
 
   useEffect(() => {
     setInnerVal(value)
   }, [value])
+
+  useEffect(() => {
+    if (!open && inputRef.current) {
+      inputRef.current.value = ''
+      setSearchTerm('')
+    }
+  }, [open])
 
   return (
     <div className="w-full relative" ref={divRef}>
@@ -51,19 +83,23 @@ export default forwardRef<HTMLDivElement, SelectProps>(function Select(
         )}
         onClick={() => {
           setOpen(true)
-          inputRef.current?.focus()
+          selectDivRef.current?.focus()
         }}
         ref={ref}
       >
-        {!innerVal ? (
-          placeholder && (
-            <p className="text-gray-400 truncate text-sm">{placeholder}</p>
-          )
-        ) : (
-          <div className="truncate">
-            {options[options.findIndex((it) => it.value === innerVal)].label}
-          </div>
-        )}
+        <input
+          ref={inputRef}
+          className={clsx(
+            'w-full font-medium outline-none border-none bg-transparent',
+            innerVal ? 'placeholder:text-gray-900' : 'placeholder:text-sm'
+          )}
+          placeholder={
+            options[options.findIndex((it) => it.value === innerVal)]?.label ??
+            placeholder
+          }
+          onChange={handleSearch}
+          readOnly={!enableSearch}
+        />
         <MdKeyboardArrowDown
           className={clsx(
             'absolute top-1/2 right-1 -translate-y-1/2 text-lg transition-transform',
@@ -73,23 +109,29 @@ export default forwardRef<HTMLDivElement, SelectProps>(function Select(
       </div>
       {open && (
         <ul className="absolute max-h-[500px] top-[calc(100%+5px)] left-0 w-full rounded-lg shadow bg-white z-10 overflow-y-auto">
-          {options.map((it, idx) => (
-            <li
-              key={idx}
-              className={clsx(
-                'px-2 py-1.5 font-semibold text-gray-600 text-sm hover:bg-gray-100 hover:text-gray-900 transition-colors cursor-pointer',
-                selectedOptionsIdx === idx &&
-                  'bg-blue-500 text-white hover:bg-blue-400 hover:text-white'
-              )}
-              onClick={() => {
-                setInnerVal(it.value)
-                setOpen(false)
-                onChange(it.value)
-              }}
-            >
-              {it.label}
-            </li>
-          ))}
+          {derivedOptions.length ? (
+            derivedOptions.map((it, idx) => (
+              <li
+                key={idx}
+                className={clsx(
+                  'px-2 py-1.5 font-semibold text-gray-600 text-sm hover:bg-gray-100 hover:text-gray-900 transition-colors cursor-pointer',
+                  selectedOptionsIdx === idx &&
+                    'bg-blue-500 text-white hover:bg-blue-400 hover:text-white'
+                )}
+                onClick={() => {
+                  setInnerVal(it.value)
+                  setOpen(false)
+                  onChange(it.value)
+                }}
+              >
+                {it.label}
+              </li>
+            ))
+          ) : emptyContent ? (
+            emptyContent
+          ) : (
+            <div className="p-2 text-sm font-medium text-center">Empty</div>
+          )}
         </ul>
       )}
     </div>
