19d1f632e53ffe09aa9a4282f8edc661a7f96a19
blackparadise0407 <blackparadise0407@gmail.com>
2022-10-12 17:01:21 +0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -9,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
